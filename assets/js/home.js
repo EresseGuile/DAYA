6025688e1ad1bcaa40c3a2f658d0b7c00fa9c04a
eresse115 <eresse115@gmail.com>
2019-09-07 09:29:46 -0500

    hello

---
+++ assets/js/home.js
@@ -0,0 +1,26 @@
+let frutas = ["mango", "cereza", "manzana"];
+let calificaciones = [10, 5, 6.7, 8, 5.9];
+/*
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+
+for (let i = 0; i < calificaciones.length; i++) {
+  console.log(calificaciones[i]);
+  if (calificaciones[i] >= 6) {
+    console.log("Aprobado");
+  } else {
+    console.log("Reprobado");
+  }
+}
+
+frutas.push("limón");
+
+  for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+  }
+*/
+///objeto
+let producto = { nombre: "mango", peso: "1kg", costo: "$20" };
+
+console.log(producto.nombre, producto.peso, producto.costo);
